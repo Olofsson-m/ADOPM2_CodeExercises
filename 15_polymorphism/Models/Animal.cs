5e97a8292864f806f718b536d69c8e9be41a6d27
Olofsson.m <maxolofsson97@gmail.com>
2025-12-03 14:18:52 +0100

    Did exercise 15.

--- 15_polymorphism/Models/Animal.cs
+++ 15_polymorphism/Models/Animal.cs
@@ -13,10 +13,14 @@ public class Animal: ISeed<Animal>
 	public int Age { get; set; }
 	public string Name { get; set; }
 
+	public virtual string MakeSound()
+    {
+        return "Generic animal sound";
+    }
 	public override string ToString() => $"{Name} the {Mood} {Age}yr";
 	
 	public bool Seeded {get; set;} = false;
-	public Animal Seed(SeedGenerator _seeder)
+	public virtual Animal Seed(SeedGenerator _seeder)
 	{
 		Mood = _seeder.FromEnum<AnimalMood>();
 		Age = _seeder.Next(0, 10);
@@ -35,11 +39,113 @@ public class Animal: ISeed<Animal>
 
 public class NordicAnimal: Animal, ISeed<NordicAnimal>
 {
-    public new NordicAnimal Seed(SeedGenerator _seeder)
+	public NordicAnimalKind Kind {get; set;}
+	public bool CanSwim {get; set;}
+    public override string ToString()
+    {
+		return $"{base.ToString()} Kind: {Kind} CanSwim: {CanSwim}";
+    }
+    public override string MakeSound()
+    {
+        if(Kind == NordicAnimalKind.Moose) return $"{Kind} Bellow!";
+		else if(Kind == NordicAnimalKind.Bear) return $"{Kind} Roar!";
+		else if(Kind == NordicAnimalKind.Wolf) return $"{Kind} Howl!";
+		else return $"{Kind} makes animal sound";
+    }
+    public override NordicAnimal Seed(SeedGenerator _seeder)
 	{
 		base.Seed(_seeder);
+		Kind = _seeder.FromEnum<NordicAnimalKind>();
+		CanSwim = _seeder.Bool;
 		return this;
 	}
+	public NordicAnimal() {  }
+	public NordicAnimal(NordicAnimal copy) : base(copy)
+    {
+        Kind = copy.Kind;
+		CanSwim = copy.CanSwim;
+    }
 }
 
+public class AfricanAnimal : Animal, ISeed<AfricanAnimal>
+{
+    public AfricanAnimalKind Kind {get; set;}
+	public decimal WeightKg {get; set;}
+
+    public override string MakeSound()
+    {
+        if(Kind == AfricanAnimalKind.Lion) return $"The {Kind} Roar!";
+        else if(Kind == AfricanAnimalKind.Elephant) return $"The {Kind} Trumpet!";
+        else if(Kind == AfricanAnimalKind.Monkey) return $"The {Kind} Screech!";
+		return $"{Kind} make animal sound..";
+    }
+    public override string ToString()
+    {
+        return $"{base.ToString()} Kind {Kind} Weight {WeightKg}kg";
+    }
+	public override AfricanAnimal Seed(SeedGenerator _seeder)
+    {
+		base.Seed(_seeder);
+		Kind = _seeder.FromEnum<AfricanAnimalKind>();
+		WeightKg = _seeder.NextDecimal(50, 5000);
+        return this;
+    }
+
+	public AfricanAnimal() {}
+	public AfricanAnimal(AfricanAnimal copy): base(copy)
+    {
+        Kind = copy.Kind;
+		WeightKg = copy.WeightKg;
+    }
+}
+
+public class HunterBird : Animal, ISeed<HunterBird>
+{
+    public HunterBirdKind Kind {get; set;}
+	public int WingspanCm {get; set;}
+	public bool CanHuntAtNight {get; set;}
+    public override string MakeSound()
+    {
+        return $"{Kind} makes a Screech!";
+    }
+    public override string ToString()
+    {
+        return $"{base.ToString()} Kind: {Kind} Wingspan {WingspanCm}cm";
+    }
+	public override HunterBird Seed(SeedGenerator _seeder)
+    {
+		base.Seed(_seeder);
+		Kind = _seeder.FromEnum<HunterBirdKind>();
+		WingspanCm = _seeder.Next(50, 250);
+		CanHuntAtNight = _seeder.Bool;
+        return this;
+    }
+
+	public HunterBird() {}
+	public HunterBird(HunterBird copy): base(copy)
+    {
+        Kind = copy.Kind;
+		WingspanCm = copy.WingspanCm;
+		CanHuntAtNight = copy.CanHuntAtNight;
+    }
+
+	public HunterBird Hunt()
+    {
+        Mood = AnimalMood.Quick;
+		System.Console.WriteLine($"{Name} is hunting and is {Mood}");
+		return this;
+    }
+	public HunterBird Fly()
+    {
+        Mood = AnimalMood.Happy;
+		System.Console.WriteLine($"{Name} is flying and {Mood}");
+		return this;
+    }
+	public HunterBird Rest()
+    {
+        Mood = AnimalMood.Sleepy;
+		System.Console.WriteLine($"{Name} is resting and is {Mood}");
+		return this;
+    }
+}
 
--- 15_polymorphism/Program.cs
+++ 15_polymorphism/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Seido.Utilities.SeedGenerator;
 using _15_polymorphism.Models;
+using System.Runtime.InteropServices;
 
 Console.WriteLine("Hello Polymorphism!");
 
@@ -13,3 +14,56 @@ foreach (var a in animals)
 {
     System.Console.WriteLine(a);
 }
+
+var nordicAnimal = new NordicAnimal().Seed(seeder);
+
+System.Console.WriteLine(nordicAnimal.MakeSound());
+
+var Zoo = new Zoo();
+var nordicAnimals = seeder.ItemsToList<NordicAnimal>(5);
+var africanAnimals = seeder.ItemsToList<AfricanAnimal>(5);
+var hunterBirds = seeder.ItemsToList<HunterBird>(5);
+
+foreach(var n in nordicAnimals)
+{
+    Zoo.ListOfAnimal.Add(n);
+}
+foreach(var a in africanAnimals)
+{
+    Zoo.ListOfAnimal.Add(a);
+}
+foreach(var h in hunterBirds)
+{
+    Zoo.ListOfAnimal.Add(h);
+}
+
+Console.WriteLine(Zoo.ToString());
+
+// foreach (var item in Zoo.ListOfAnimal)
+// {
+//     System.Console.WriteLine(item.MakeSound());
+// }
+
+void ReseedAllAnimals(List<Animal> animals, SeedGenerator seeder)
+{
+    foreach(var a in animals)
+    {
+        a.Seed(seeder);
+    }
+}
+
+ReseedAllAnimals(Zoo.ListOfAnimal, seeder);
+
+System.Console.WriteLine(Zoo.ToString());
+/*
+var nordicAnimal = new NordicAnimal().Seed(seeder);
+System.Console.WriteLine(nordicAnimal);
+
+var africanAnimal = new AfricanAnimal().Seed(seeder);
+System.Console.WriteLine(africanAnimal);
+
+var hunterBird = new HunterBird().Seed(seeder);
+System.Console.WriteLine(hunterBird);
+
+hunterBird.Hunt().Fly().Rest();
+*/
